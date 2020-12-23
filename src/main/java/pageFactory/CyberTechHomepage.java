0ce595a8e60a128208aaf147326282b75eaf003c
rabin2004 <55056465+rabin2004@users.noreply.github.com>
2020-12-22 21:01:01 -0500

    fresh new push - Rabin!

---
+++ src/main/java/pageFactory/CyberTechHomepage.java
@@ -0,0 +1,168 @@
+package pageFactory;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import techBase.BaseClass;
+
+public class CyberTechHomepage extends BaseClass  {
+	
+	// *** SELECT COURSES - USMAN ***
+	
+	//@FindBy(xpath = "//a[contains(text(),'Select Courses')]")
+	//WebElement selectCourses;
+	By selectCourses = By.xpath("//a[contains(text(),'Select Courses')]");
+	
+	//@FindBy(id = "mega-menu-item-609")
+			//WebElement marketingClass_1;
+			By marketingClass_1 = By.id("mega-menu-item-609");
+			
+			//@FindBy(id = "mega-menu-item-610")
+			//WebElement marketingClass_2;
+			By marketingClass_2 = By.id("mega-menu-item-610");
+			
+			//@FindBy(id = "mega-menu-item-611")
+			//WebElement marketingClass_3;
+			By marketingClass_3 = By.id("mega-menu-item-611");
+			
+			//@FindBy(id = "mega-menu-item-612")
+			//WebElement marketingClass_4;
+			By marketingClass_4 = By.id("mega-menu-item-612");
+			
+			By project_manage_Class_1 = By.id("mega-menu-item-620");
+			
+			By businessClass_1 = By.id("mega-menu-item-618");
+			
+			By businessClass_2 = By.id("mega-menu-item-621");
+			
+			By businessClass_3 = By.id("mega-menu-item-623");
+			
+			By financeClass_1 = By.id("mega-menu-item-619");
+			
+			By mathClass_1 = By.id("mega-menu-item-617");
+			
+			By salesClass_1 = By.id("mega-menu-item-622");
+			
+			//@FindBy(name = "name")
+			//WebElement FirstName;
+			By FirstName = By.name("name");
+			
+			//@FindBy(name = "email")
+			//WebElement Email;
+			By Email = By.name("email");
+			
+			//@FindBy(name = "mobile")
+			//WebElement PhoneNumber;
+			By PhoneNumber = By.name("mobile");
+			
+		    //@FindBy(xpath = "//button[contains(text(),'Join Demo Class')]")
+			//WebElement submitBtn;
+			By submitBtn = By.xpath("//button[contains(text(),'Join Demo Class')]");
+			
+			
+	// *** BLOG - CHAMIKA ***
+	//@FindBy(xpath = "//a[contains(text(),'Blog')]")
+	//WebElement blog; 
+	By blog = By.xpath("//a[contains(text(),'Blog')]");
+	
+	// *** FOOTER LINKS - JASVIR ***
+	
+	//*** SIGN UP - BINAY ***
+	
+	
+	public CyberTechHomepage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	// *** SELECT COURSES - USMAN ***
+	public void clickSelectCourses() {
+    driver.findElement(selectCourses).click();
+		
+	}
+	
+	public void NutritionClass_1() {
+		driver.findElement(marketingClass_1).click();
+	}
+		
+	public void nameTxtField(String name) {
+		driver.findElement(FirstName).sendKeys(name);
+	}
+		
+    public void emailTxtField(String EmailAddress) {
+    	driver.findElement(Email).sendKeys(EmailAddress);
+    }
+		
+    public void phoneTxtField(String Phone) {
+    	driver.findElement(PhoneNumber).sendKeys(Phone);
+		
+	}
+	
+	public void NutritionClass_2() {
+		driver.findElement(marketingClass_2).click();
+		
+		
+		
+	}
+	
+	public void PHPclass() {
+		driver.findElement(marketingClass_3).click();
+		
+		
+	}
+	
+	public void WordPress() {
+		driver.findElement(marketingClass_4).click();
+			
+	}
+	
+	public void Learn3DModelling() {
+		driver.findElement(project_manage_Class_1).click();
+	}
+	
+	public void PhotoshopTraining() {
+		driver.findElement(businessClass_1).click();
+	}
+	
+	public void PracticalSales() { 
+		driver.findElement(businessClass_2).click();
+	}
+	
+	public void FinancialAnalyst() {
+		driver.findElement(businessClass_3).click();
+		
+	}
+	
+	public void UserExperienceDesign() {
+		driver.findElement(financeClass_1).click();
+	}
+	
+	public void JavaScript() {
+		driver.findElement(mathClass_1).click();
+	}
+	
+	public void TrelloFundamentals() {
+		driver.findElement(salesClass_1).click();
+	}
+	
+	public void JoinDemo() {
+		driver.findElement(submitBtn).click();
+		
+	}
+	// *** BLOG - CHAMIKA ***
+	public void clickBlog() {
+		driver.findElement(blog).click();
+	}
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+
+
---
+++ src/test/java/step_definitions/SelectCourses.java
@@ -0,0 +1,161 @@
+package step_definitions;
+
+
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pageFactory.CyberTechHomepage;
+import techBase.BaseClass;
+
+public class SelectCourses extends BaseClass {
+	CyberTechHomepage cb;
+	
+	
+	public SelectCourses() {
+		super();
+		cb = new CyberTechHomepage(); 
+		
+		}
+	
+	@Given("user is in the Cyber Tech homepage")
+	public void user_is_in_the_cyber_tech_homepage() {
+		initialization();
+	    
+	}
+
+	@When("user clicks on Select Courses")
+	public void user_clicks_on_select_courses() {
+		cb.clickSelectCourses();
+	    
+	}
+
+	@Then("user clicks on first course in the Marketing Category")
+	public void user_clicks_on_first_course_in_the_marketing_category() {
+	    cb.NutritionClass_1();
+	}
+
+	@And("user provides name, email, phone number to join the demo class")
+	public void user_provides_name_email_phone_number_to_join_the_demo_class() {
+	   
+		// Marketing course 1
+		cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	}
+	    
+	
+	@Then("user continues to repeat the same steps for the rest of the courses in all course categories")
+	public void user_continues_to_repeat_the_same_steps_for_the_rest_of_the_courses_in_all_course_categories() {
+		
+		// Marketing course 2
+	    cb.clickSelectCourses();
+	    cb.NutritionClass_2();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	  
+	    // Marketing course 3
+	    cb.clickSelectCourses();
+	    cb.PHPclass();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	   
+	    // Marketing course 4 
+	    cb.clickSelectCourses();
+	    cb.WordPress();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	    
+	    // Project Management course 1
+	    cb.clickSelectCourses();
+	    cb.Learn3DModelling();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	    
+	    // Business course 1
+	    cb.clickSelectCourses();
+	    cb.PhotoshopTraining();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	    
+	 // Business course 2
+	    cb.clickSelectCourses();
+	    cb.PracticalSales();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	    
+	 // Business course 3
+	    cb.clickSelectCourses();
+	    cb.FinancialAnalyst();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	    
+	    // Finance course 1
+	    cb.clickSelectCourses();
+	    cb.UserExperienceDesign();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	    
+	    // Math course 1
+	    cb.clickSelectCourses();
+	    cb.JavaScript();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	    
+	    // Sales course 1
+	    cb.clickSelectCourses();
+	    cb.TrelloFundamentals();
+	    cb.nameTxtField(prop.getProperty("name"));
+	    cb.emailTxtField(prop.getProperty("EmailAddress"));
+	    cb.phoneTxtField(prop.getProperty("Phone"));
+	    cb.JoinDemo();
+	    
+	    
+	    
+	    
+	    
+	
+	}
+	
+	@Then("user closes browser")
+	public void user_closes_browser() {
+		
+		tearDown();
+	}
+	
+}
+	
+
+
+
+
+
+
+	
+		
+		
+	  
+	    
+	
+
+
